6238b3f4a8092cfa8e1b15f9fc92df83264902a3
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-04-28 18:50:53 +0000

    Add update from and partial file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -12,12 +14,26 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
+    @article = Article.new( article_params )
     @article.save
 
     redirect_to article_path(@article)
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
