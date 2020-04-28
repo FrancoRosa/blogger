173e55036ed4bdf54d73289e908f084d1e7cd310
FrancoRosa <km115.franco@gmail.com>
2020-04-28 16:59:00 -0500

    Add edit comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,4 +21,16 @@ class CommentsController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to article_path(@article)
+  end
 end
